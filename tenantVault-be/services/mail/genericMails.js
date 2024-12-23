18756ea6049492339fd054dcb530601b04777407
Vijender <vijender.kumar@arthmate.co.in>
2024-12-23 18:16:17 +0530

    Removed extra fcuntion

--- tenantVault-be/services/mail/genericMails.js
+++ tenantVault-be/services/mail/genericMails.js
@@ -30,17 +30,6 @@ const genericMails = (type, data) => {
             Regards,<br>
             TENANT VAULT PVT LIMITED</div>`;
       break;
-    // case "passwordreset":
-    //   return `<div>Hi ${data.username},<br><br>
-    //         Your password successfully changed.<br>
-    //         PFB, the new Admin credentials for tenant vault dashboard.<br><br>
-    //         <label><b>URL : </b></label>${process.env.TENANT_VAULT_DASH_URL}<br>
-    //         <label><b>email : </b></label>${data.email}<br>
-    //         <label><b>username : </b></label>${data.username}<br>
-    //         <label><b>password : </b></label>${data.confirmPassword}<br><br>
-    //         Regards,<br>
-    //         ARTHMATETECH PRIVATE LIMITED</div>`;
-    //   break;
     default:
       break;
   }
